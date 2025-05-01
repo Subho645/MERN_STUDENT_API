51004dcf3f13489c2e6395f8ae1421bff3dfce65
Subho Banerjee <banerjeesubho33@gmail.com>
2025-05-01 14:05:04 +0530

    Added root route

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", studentRoutes);
+app.get('/', (req, res) => {
+  res.send('Student API is live and working!');
+});
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
